a1a19c826e7bf639cabd47133e0c457c41515b88
Bluelightzero <bluelightzero@gmail.com>
2013-06-17 20:14:33 +0100

    OOps, forgot build files

---
+++ bin/ReadingBrowser-release-unsigned.apk.d
@@ -0,0 +1,4 @@
+/home/jay/proj/android/ReadingBrowser/bin/ReadingBrowser-release-unsigned.apk : \
+/home/jay/proj/android/ReadingBrowser/bin/ReadingBrowser.ap_ \
+/home/jay/proj/android/ReadingBrowser/bin/classes.dex \
+/home/jay/proj/android/android-sdk-linux/tools/support/annotations.jar \
---
+++ gen/me/bluelightzero/readingbrowser/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package me.bluelightzero.readingbrowser;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
